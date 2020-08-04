5a447b0beeb4a753788a21bc39a73052c03084c2
Eli Kristina <eli_kristina@yahoo.com>
2020-08-04 17:14:38 +0700

    create index page

---
+++ index.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+import "net/http"
+import "html/template"
+
+func main() {
+	http.Handle("/static/",
+		http.StripPrefix("/static/",
+			http.FileServer(http.Dir("assets"))))
+
+	fmt.Println("server start")
+	http.HandleFunc("/", home)
+	http.ListenAndServe(":9000", nil)
+}
+
+func home(w http.ResponseWriter, r *http.Request) {
+	var tmpl = template.Must(template.New("index").ParseFiles("template/index.html"))
+	if err := tmpl.Execute(w, nil); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
